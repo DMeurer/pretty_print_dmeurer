5a83cb369b04b8323b839853d05a95ad44bcbbac
Dominik Meurer <dom.meurer@gmx.de>
2024-07-16 10:53:50 +0200

    init

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+from src import pretty_print_dmeurer
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pretty_print_dmeurer.color_demo()
+    print(pretty_print_dmeurer.Colors.FG.green + "HEY")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
